c438de6fdd6e87cee9a910791677f15c5c19051a
jasonjasontu <jasonpsu@163.com>
2016-01-02 20:06:18 -0500

    finish the get fuction for create public event

--- app/src/main/java/com/ac/behrendapp/whattodo/CreatePublicEvent.java
+++ app/src/main/java/com/ac/behrendapp/whattodo/CreatePublicEvent.java
@@ -2,6 +2,7 @@ package com.ac.behrendapp.whattodo;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.net.Uri;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +13,11 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.w3c.dom.Text;
 
@@ -20,8 +26,13 @@ import java.util.Calendar;
 public class CreatePublicEvent extends AppCompatActivity {
 
     private Spinner locSpinner;
-    private EditText nameET,startTimeET,endTimeET,desET;
-    private TextView dateET,ETstartTime,ETendTime;
+    private EditText nameET, startTimeET, endTimeET, desET;
+    private TextView dateET, ETstartTime, ETendTime;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +43,20 @@ public class CreatePublicEvent extends AppCompatActivity {
         addItemsToLocSpinner();
 
 
-
-
-
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     // This function is to initialize all the field variable and wait for input
-    public void initialize(){
-        locSpinner=(Spinner)findViewById(R.id.Loc_Spinner);
-        nameET=(EditText)findViewById(R.id.eventName);
+    public void initialize() {
+        locSpinner = (Spinner) findViewById(R.id.Loc_Spinner);
+        nameET = (EditText) findViewById(R.id.eventName);
         //dateET=(EditText)findViewById(R.id.date);
-        startTimeET=(EditText)findViewById(R.id.start_time);
-        endTimeET=(EditText)findViewById(R.id.end_time);
-        desET=(EditText)findViewById(R.id.des);
-        dateET=(TextView)findViewById(R.id.date);
+        startTimeET = (EditText) findViewById(R.id.start_time);
+        endTimeET = (EditText) findViewById(R.id.end_time);
+        desET = (EditText) findViewById(R.id.des);
+        dateET = (TextView) findViewById(R.id.date);
     }
 
     //This function is to pop out a dialog to pick date when click on the text field of date
@@ -61,7 +71,7 @@ public class CreatePublicEvent extends AppCompatActivity {
             DatePickerDialog mDatePicker;
             mDatePicker = new DatePickerDialog(CreatePublicEvent.this, new DatePickerDialog.OnDateSetListener() {
                 public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
-                    selectedmonth+=1;
+                    selectedmonth += 1;
                     ET_dateTime.setText("" + selectedmonth + "/" + selectedday + "/" + selectedyear);
                 }
             }, mYear, mMonth, mDay);
@@ -71,64 +81,64 @@ public class CreatePublicEvent extends AppCompatActivity {
     }
 
     //This function is to pop out a dialog to pick time when click on the text field of StartTime
-    public void onStartTimeClick(View v){
-        final EditText ET_startTime = (EditText)findViewById(R.id.start_time);
-        if(v.getId()==R.id.start_time){
-            Calendar currentTime=Calendar.getInstance();
-            int curr_hour=currentTime.HOUR_OF_DAY;
-            int curr_minute=currentTime.MINUTE;
+    public void onStartTimeClick(View v) {
+        final EditText ET_startTime = (EditText) findViewById(R.id.start_time);
+        if (v.getId() == R.id.start_time) {
+            Calendar currentTime = Calendar.getInstance();
+            int curr_hour = currentTime.HOUR_OF_DAY;
+            int curr_minute = currentTime.MINUTE;
 
             TimePickerDialog TimePicker1;
 
 
-            TimePicker1=new TimePickerDialog(CreatePublicEvent.this,
+            TimePicker1 = new TimePickerDialog(CreatePublicEvent.this,
                     new TimePickerDialog.OnTimeSetListener() {
                         @Override
                         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                            ET_startTime.setText( hourOfDay + ":" + minute);
+                            ET_startTime.setText(hourOfDay + ":" + minute);
                         }
-                    },curr_hour,curr_minute,false);
+                    }, curr_hour, curr_minute, false);
             TimePicker1.setTitle("Select Time");
             TimePicker1.show();
         }
     }
 
     //This function is to pop out a dialog to pick time when click on the text field of EndTime
-    public void onEndTimeClick(View v){
-        final EditText ET_endTime = (EditText)findViewById(R.id.end_time);
-        if(v.getId()==R.id.end_time){
-            Calendar currentTime=Calendar.getInstance();
-            int curr_hour=currentTime.HOUR_OF_DAY;
-            int curr_minute=currentTime.MINUTE;
+    public void onEndTimeClick(View v) {
+        final EditText ET_endTime = (EditText) findViewById(R.id.end_time);
+        if (v.getId() == R.id.end_time) {
+            Calendar currentTime = Calendar.getInstance();
+            int curr_hour = currentTime.HOUR_OF_DAY;
+            int curr_minute = currentTime.MINUTE;
 
             TimePickerDialog TimePicker1;
 
 
-            TimePicker1=new TimePickerDialog(CreatePublicEvent.this,
+            TimePicker1 = new TimePickerDialog(CreatePublicEvent.this,
                     new TimePickerDialog.OnTimeSetListener() {
                         @Override
                         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                            ET_endTime.setText( hourOfDay + ":" + minute);
+                            ET_endTime.setText(hourOfDay + ":" + minute);
                         }
-                    },curr_hour,curr_minute,false);
+                    }, curr_hour, curr_minute, false);
             TimePicker1.setTitle("Select Time");
             TimePicker1.show();
         }
     }
 
     //This function is going to save the user input into DB when the next Button is clicked
-    public void onNextClick(View v){
+    public void onNextClick(View v) {
 
-        if(v.getId()==R.id.nextButton){
+        if (v.getId() == R.id.nextButton) {
 
             //get data from fields
-            int spinnerPos=locSpinner.getSelectedItemPosition();
-            String loc=locSpinner.getItemAtPosition(spinnerPos).toString();
-            String name=nameET.getText().toString();
-            String date=dateET.getText().toString();
-            String startTime=startTimeET.getText().toString();
-            String endTime=endTimeET.getText().toString();
-            String des=desET.getText().toString();
+            int spinnerPos = locSpinner.getSelectedItemPosition();
+            String loc = locSpinner.getItemAtPosition(spinnerPos).toString();
+            String name = nameET.getText().toString();
+            String date = dateET.getText().toString();
+            String startTime = startTimeET.getText().toString();
+            String endTime = endTimeET.getText().toString();
+            String des = desET.getText().toString();
 
 
           /*
@@ -148,17 +158,11 @@ public class CreatePublicEvent extends AppCompatActivity {
                         */
 
 
-
-
-
-
-
-
         }
 
     }
 
-    public String getName(String name){
+    public String getName(String name) {
         //A. This function is to check whether the name input is valid
         //      and will return a boolean value.
         //B. If the name is not correct, it will pop out a dialog to display
@@ -166,10 +170,14 @@ public class CreatePublicEvent extends AppCompatActivity {
         //C. else return name;
         // Requirement:
         //  1) not empty
-        return name;
+        if (name == null) {
+            alertmessage("Your name cannot be empty");
+            return null;
+        } else
+            return name;
     }
 
-    public int getDate(String date){
+    public int getDate(String date) {
         //A. This function is to check whether the date input is valid.
         //      and will return a boolean value
         //B. If the name is not correct, it will pop out a dialog to display
@@ -180,11 +188,20 @@ public class CreatePublicEvent extends AppCompatActivity {
         //  2)the date has exact format mm/dd/yyyy
         //  * String "mm/dd/yyyy" => int "mmddyyyy"
         //  3)the date is some day after today
-        return -1;
-
+        int result = -1;
+        if (date == null) {
+            alertmessage("Date cannot be empty");
+        } else {
+            String[] str = date.split("/");
+            int day = Integer.parseInt(str[0]);
+            int month = Integer.parseInt(str[1]);
+            int year = Integer.parseInt(str[2]);
+            result = day * 1000000 + month * 10000 + year;
+        }
+        return result;
     }
 
-    public int getStartTime(String startTime){
+    public int getStartTime(String startTime) {
         //A. This function is to check whether the date input is vaild
         //      and will a boolean value
         //B. If the name is not correct, it will pop out a dialog to display
@@ -193,10 +210,19 @@ public class CreatePublicEvent extends AppCompatActivity {
         //D. Requirement:
         //  1)not empty
         //  2)the time has exactly format hh:mm
-        return -1;
+        int result = -1;
+        if (startTime == null) {
+            alertmessage("Start time cannot be empty");
+        } else {
+            String[] str = startTime.split("/");
+            int hour = Integer.parseInt(str[0]);
+            int minute = Integer.parseInt(str[1]);
+            result = hour*100 + minute;
+        }
+        return result;
     }
 
-    public int getEndTime(String startTime, String endTime){
+    public int getEndTime(String startTime, String endTime) {
         //A. This function is to check whether the date input is vaild
         //      and will a boolean value
         //B. If the name is not correct, it will pop out a dialog to display
@@ -206,12 +232,27 @@ public class CreatePublicEvent extends AppCompatActivity {
         //  1)not empty
         //  2)the time has exactly format hh:mm
         // 3)the endTime should be some time after startTime
-        return -1;
+        int result = -1;
+        if (endTime == null)
+            alertmessage("End time cannot be empty");
+        else if (endTime.compareTo(startTime) < 0)
+            alertmessage("End time cannot be earlier than starting time");
+        else {
+            String[] str = endTime.split("/");
+            int hour = Integer.parseInt(str[0]);
+            int minute = Integer.parseInt(str[1]);
+            result = hour*100 + minute;
+        }
+        return result;
+    }
+
+    private void alertmessage(String message) {
+        Toast toast = Toast.makeText(getApplicationContext(), message,Toast.LENGTH_LONG);
     }
 
     // This function is to add items on the location Spinner
-    public void addItemsToLocSpinner(){
-        locSpinner = (Spinner)findViewById(R.id.Loc_Spinner);
+    public void addItemsToLocSpinner() {
+        locSpinner = (Spinner) findViewById(R.id.Loc_Spinner);
         ArrayAdapter<CharSequence> locSpinnerAdapter =
                 ArrayAdapter.createFromResource(this,
                         R.array.LOCATIONS,
@@ -221,17 +262,18 @@ public class CreatePublicEvent extends AppCompatActivity {
         locSpinner.setAdapter(locSpinnerAdapter);
     }
 
+
     // TBC
-    public void addListenerToLocSpinner(){
-            //https://www.youtube.com/watch?v=OY8dRInKaqY
+    public void addListenerToLocSpinner() {
+        //https://www.youtube.com/watch?v=OY8dRInKaqY
         final String loc;
-        locSpinner=(Spinner)findViewById(R.id.Loc_Spinner);
+        locSpinner = (Spinner) findViewById(R.id.Loc_Spinner);
         locSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String itemSelectedInSpinner =
                         parent.getItemAtPosition(position).toString();
-                        //TextView test=(TextView)findViewById(R.id.test);
+                //TextView test=(TextView)findViewById(R.id.test);
             }
 
             @Override
@@ -241,4 +283,44 @@ public class CreatePublicEvent extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "CreatePublicEvent Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.ac.behrendapp.whattodo/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "CreatePublicEvent Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.ac.behrendapp.whattodo/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
